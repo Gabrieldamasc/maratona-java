6f2bee25585989f2e44a300eb768359872b52127
Gabrieldamasceno <juragabriel21@gmail.com>
2025-02-28 11:21:33 -0300

    initial commit

---
+++ src/academy/devdojo/maratonajava/javacore/Pwrappers/test/WrapperTest01.java
@@ -0,0 +1,23 @@
+package academy.devdojo.maratonajava.javacore.Pwrappers.test;
+
+public class WrapperTest01 {
+    public static void main(String[] args) {
+        byte byteP = 1;
+        int intP = 10;
+        short shortP = 1;
+        float floatP = 10f;
+        long longP = 10L;
+        double doubleP = 10d;
+        boolean booleanP = false;
+        char charP = 'w';
+
+        Byte byteW = 1;
+        Integer intW = 10;
+        Short shortW = 1;
+        Float floatW = 10f;
+        Long longW = 10L;
+        Double doubleW = 10d;
+        Boolean booleanW = false;
+        Character charW = 'W';
+    }
+}
