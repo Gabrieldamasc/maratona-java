56ea065ac6429e180c63ee8b715de60049dfb0c7
Gabrieldamasceno <juragabriel21@gmail.com>
2025-03-02 16:54:06 -0300

    study att

--- src/academy/devdojo/maratonajava/javacore/Rdatas/test/LocalDateTimeTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Rdatas/test/LocalDateTimeTest01.java
@@ -6,15 +6,12 @@ import java.time.LocalTime;
 
 public class LocalDateTimeTest01 {
     public static void main(String[] args) {
-        LocalDateTime ldt1 = LocalDateTime.now();
-        LocalTime lt1 = LocalTime.parse("18:48:50");
-        LocalDate ld1 = LocalDate.parse("2006-10-10");
-        LocalDateTime time = lt1.atDate(ld1);
-        LocalDateTime dateAndTime = ld1.atTime(lt1);
-        System.out.println(ldt1);
-        System.out.println(lt1);
-        System.out.println(ld1);
-        System.out.println(time);
-        System.out.println(dateAndTime);
+        LocalDateTime dateNow = LocalDateTime.now();
+        LocalDate localDate1 = LocalDate.parse("2006-10-06");
+        LocalTime localTime1 = LocalTime.parse("18:30:33");
+        LocalDateTime ldt1 = localTime1.atDate(localDate1);
+        if (ldt1.isBefore(dateNow)){
+            System.out.println("Saving requisitions from: "+ localDate1+ " to " +dateNow);
+        }
     }
 }
