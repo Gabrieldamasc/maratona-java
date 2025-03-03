bc349c722193e5dffeaeb1b885bd7b30b01dfd45
Gabrieldamasceno <juragabriel21@gmail.com>
2025-03-03 20:53:10 -0300

    study att

---
+++ src/lista/execercicios/lista10/Lista10.java
@@ -0,0 +1,29 @@
+package lista.execercicios.lista10;
+
+import java.util.Scanner;
+
+public class Lista10 {
+    public static void main(String[] args) {
+        Scanner userIn = new Scanner(System.in);
+        System.out.println("Type the first number: ");
+        int userNumIn1 = userIn.nextInt();
+        System.out.println("Type the second number: ");
+        int userNumIn2 = userIn.nextInt();
+        System.out.println("Type the third number: ");
+        int userNumIn3 = userIn.nextInt();
+
+        int sumNum = userNumIn1 + userNumIn2;
+
+        System.out.println(sumNumAct(sumNum,userNumIn3));
+
+
+    }
+
+    public static boolean sumNumAct(int sumNum, int userNumIn3){
+        if (sumNum == userNumIn3){
+            return true;
+        }else {
+            return false;
+        }
+    }
+}
