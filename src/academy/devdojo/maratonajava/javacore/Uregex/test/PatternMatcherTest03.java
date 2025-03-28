70253775aa1d98f24b61f94b3db42af33ee974d6
Gabrieldamasceno <juragabriel21@gmail.com>
2025-03-28 14:52:57 -0300

    study att

--- src/academy/devdojo/maratonajava/javacore/Uregex/test/PatternMatcherTest03.java
+++ src/academy/devdojo/maratonajava/javacore/Uregex/test/PatternMatcherTest03.java
@@ -3,10 +3,10 @@ package academy.devdojo.maratonajava.javacore.Uregex.test;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class PatternMatcherTest02 {
+public class PatternMatcherTest03 {
     public static void main(String[] args) {
-        String texto = "f1 0xf323 0X43a x023";
-        String regex = "0[Xx][0-9a-fA-F]";
+        String texto = "12 0x 0X 0xFFABC 0x10G 0x1";
+        String regex = "0[Xx]([0-9a-fA-F])+(\\s|$)";
         Pattern compile = Pattern.compile(regex);
         Matcher matcher = compile.matcher(texto);
         System.out.println("texto:   " + texto);
