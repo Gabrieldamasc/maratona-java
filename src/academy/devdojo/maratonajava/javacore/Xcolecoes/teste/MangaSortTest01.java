84383483a5e442a358852b5c5c2b87e7d8abc15a
Gabrieldamasceno <juragabriel21@gmail.com>
2025-04-28 17:38:54 -0300

    study att

--- src/academy/devdojo/maratonajava/javacore/Xcolecoes/teste/MangaSortTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Xcolecoes/teste/MangaSortTest01.java
@@ -4,7 +4,24 @@ import academy.devdojo.maratonajava.javacore.Xcolecoes.dominio.Manga;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
+
+class MangaByNameComparator implements Comparator<Manga>{
+
+    @Override
+    public int compare(Manga manga01, Manga manga02) {
+        return manga01.getNome().compareTo(manga02.getNome());
+    }
+}
+
+class MangaByPriceComparator implements Comparator<Manga>{
+
+    @Override
+    public int compare(Manga manga01, Manga manga02) {
+        return Double.valueOf(manga01.getPreco()).compareTo(manga02.getPreco());
+    }
+}
 
 public class MangaSortTest01 {
     public static void main(String[] args) {
@@ -19,6 +36,20 @@ public class MangaSortTest01 {
         Collections.sort(mangas);
         for (Manga manga : mangas) {
             System.out.println(manga);
+        }
+
+        System.out.println("----------------------------");
+
+        Collections.sort(mangas, new MangaByNameComparator());
+        for (Manga mangaByName : mangas){
+            System.out.println(mangaByName);
+        }
+
+        System.out.println("----------------------------");
+
+        Collections.sort(mangas, new MangaByPriceComparator());
+        for (Manga mangaByPrice : mangas){
+            System.out.println(mangaByPrice);
         }
     }
 }
