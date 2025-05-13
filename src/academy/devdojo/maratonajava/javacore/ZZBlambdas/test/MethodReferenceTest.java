c6c9ef24b585bb52438a7d4662384077dba64d14
Gabrieldamasceno <juragabriel21@gmail.com>
2025-05-13 16:05:39 -0300

    study att

---
+++ src/academy/devdojo/maratonajava/javacore/ZZBlambdas/test/MethodReferenceTest.java
@@ -0,0 +1,19 @@
+package academy.devdojo.maratonajava.javacore.ZZBlambdas.test;
+
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.BiPredicate;
+
+public class MethodReferenceTest {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>(List.of("Gabriel", "Daniel", "Adriana", "Juracy"));
+        list.replaceAll(String::toLowerCase);
+        BiPredicate<List<String>, String> checkName = List::contains;
+        System.out.println(checkName.test(list, "adriana"));
+        list.replaceAll(String::toUpperCase);
+        list.sort(String::compareTo);
+        System.out.println(list);
+
+    }
+}
