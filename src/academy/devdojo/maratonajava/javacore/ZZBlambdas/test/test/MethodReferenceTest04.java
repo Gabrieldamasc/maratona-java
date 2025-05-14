191c64baf9ad5957c1fad6e393bfaabe95467a2c
Gabrieldamasceno <juragabriel21@gmail.com>
2025-05-14 15:22:37 -0300

    study att

--- src/academy/devdojo/maratonajava/javacore/ZZBlambdas/test/test/MethodReferenceTest04.java
+++ src/academy/devdojo/maratonajava/javacore/ZZBlambdas/test/test/MethodReferenceTest04.java
@@ -5,10 +5,20 @@ import academy.devdojo.maratonajava.javacore.ZZBlambdas.test.service.AnimeCompar
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.BiFunction;
+import java.util.function.Supplier;
 
 public class MethodReferenceTest04 {
     public static void main(String[] args) {
+        Supplier<AnimeComparator> animeComparatorSupplier = AnimeComparator::new;
+        AnimeComparator animeComparator = animeComparatorSupplier.get();
         List<Anime> animeList = new ArrayList<>(List.of(new Anime("Naruto", 350), new Anime("One piece", 720), new Anime("Attack on titan", 650)));
-        AnimeComparator animeComparator = new AnimeComparator();
+        animeList.sort(animeComparator::animeComparatorByTitleNonEstatic);
+        System.out.println(animeList);
+        animeList.sort(AnimeComparator::animeComparatorByEpisodes);
+        System.out.println(animeList);
+
+        BiFunction<String, Integer, Anime> stringIntegerAnimeBiFunction = Anime::new;
+        System.out.println(stringIntegerAnimeBiFunction.apply("FullMetal", 55));
     }
 }
