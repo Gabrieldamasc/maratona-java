6aca853d8a0c538d27e0d1ace1e79342d4e29b49
Gabrieldamasceno <juragabriel21@gmail.com>
2025-05-19 15:22:25 -0300

    study att

---
+++ src/academy/devdojo/maratonajava/javacore/ZZEstreams/test/StreamTest11.java
@@ -0,0 +1,27 @@
+package academy.devdojo.maratonajava.javacore.ZZEstreams.test;
+
+import academy.devdojo.maratonajava.javacore.ZZEstreams.dominio.LightNovel;
+
+import java.util.ArrayList;
+import java.util.DoubleSummaryStatistics;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class StreamTest11 {
+    private static List<LightNovel> lightNovel = new ArrayList<>(List.of(new LightNovel("Fullmetal", 4.99),
+            new LightNovel("Pokemom", 2.50),
+            new LightNovel("No Game No Life", 3.99),
+            new LightNovel("OverLord", 8.00),
+            new LightNovel("Violet Evergarden", 1.73),
+            new LightNovel("Kumo desuga", 4.00),
+            new LightNovel("Monogatari", 7.32),
+            new LightNovel("Tensei Shittara", 3.54)
+    ));
+    public static void main(String[] args) {
+        DoubleSummaryStatistics collect = lightNovel.stream().collect(Collectors.summarizingDouble(LightNovel::getPrice));
+        System.out.println(collect);
+
+        String titles = lightNovel.stream().map(LightNovel::getTitle).collect(Collectors.joining(", "));
+        System.out.println(titles);
+    }
+}
